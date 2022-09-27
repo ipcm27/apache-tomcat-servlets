d8e37f9cda8cba1d99ca3dd73ffe45f3248605b3
Igor Pierre <igorpcmiranda@gmail.com>
2022-09-26 23:07:14 -0300

    adicioando material d aaula até agora

---
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/NovaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("cadastrando nova empresa");
+		String nomeEmpresa = request.getParameter("nome");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html></body>Empresa" + nomeEmpresa + "cadastrada com sucesso</boyd></html>");
+	}
+
+}
